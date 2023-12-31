15fda17bc2104a10575dfcc88350f445ed002461
pan <2556323541@qq.com>
2023-12-31 18:48:39 +0800

    add utils package

---
+++ utils/errorx/codeerror.go
@@ -0,0 +1,71 @@
+package errorx
+
+import (
+	"errors"
+	"fmt"
+)
+
+type (
+	CodeError interface {
+		error
+		Status() int
+		Code() int
+	}
+
+	codeError struct {
+		code   int
+		status int
+		desc   string
+	}
+)
+
+func (err *codeError) Code() int {
+	return err.code
+}
+
+func (err *codeError) Status() int {
+	return err.status
+}
+
+func (err *codeError) Error() string {
+	return err.desc
+}
+
+func (err *codeError) String() string {
+	return fmt.Sprintf("Status: %d, Code: %d, Desc: %s", err.status, err.code, err.desc)
+}
+
+func NewCodeError(code, status int, desc string) CodeError {
+	return &codeError{
+		code:   code,
+		status: status,
+		desc:   desc,
+	}
+}
+
+func FromError(err error) (CodeError, bool) {
+	if err == nil {
+		return nil, false
+	}
+	var ce CodeError
+	if ok := errors.As(err, &ce); ok {
+		return ce, ok
+	}
+	return nil, false
+}
+
+func Error400(desc string) CodeError {
+	return NewCodeError(400, 400, desc)
+}
+
+func Error500(desc string) CodeError {
+	return NewCodeError(500, 500, desc)
+}
+
+func ErrorDB(err error) CodeError {
+	return NewCodeError(500, 500, fmt.Sprintf("[DB ERROR]: %v", err))
+}
+
+func ErrorAuth() CodeError {
+	return NewCodeError(401, 401, "认证失败")
+}
