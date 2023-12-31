f273b7a4756068cff12b814636a19c30ec1f7d89
pan <2556323541@qq.com>
2023-12-31 18:48:20 +0800

    project success run

---
+++ core/internal/handler/basic/ping_handler.go
@@ -0,0 +1,15 @@
+package basic
+
+import (
+	"net/http"
+
+	"github.com/pjimming/zustacm/core/internal/svc"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func PingHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("pong"))
+		httpx.Ok(w)
+	}
+}
